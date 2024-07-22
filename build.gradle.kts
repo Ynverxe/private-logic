734b7ca6df155011f8ff78e7735408d50613903c
Ynverxe <nahubar65@gmail.com>
2024-07-22 14:37:12 -0300

    ci: add maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     `kotlin-dsl`
+    id("maven-publish")
 }
 
 group = "com.github.ynverxe"
 version = "0.1.0"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
 repositories {
     gradlePluginPortal()
@@ -15,6 +21,30 @@ gradlePlugin {
         create("private-logic") {
             id = "com.github.ynverxe.private-logic"
             implementationClass = "com.github.ynverxe.privatelogic.PrivateLogicPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = project.name
+            groupId = project.group.toString()
+            version = project.version.toString()
+
+            from(components["kotlin"])
+
+            pom {
+                name = "private-logic"
+                description = "A gradle plugin to load personal logic on group projects"
+                url = "https://github.com/Ynverxe/private-logic"
+                developers {
+                    developer {
+                        id = "Ynverxe"
+                        url = "https://github.com/Ynverxe"
+                    }
+                }
+            }
         }
     }
 }
